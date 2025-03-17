eedb97a6c2fba79ff82f2ce9f8ef8107f16c012c
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2025-03-17 21:36:25 +0100

    rephrase question chain

--- src/modules/agent/chains/rephrase-question.chain.ts
+++ src/modules/agent/chains/rephrase-question.chain.ts
@@ -19,26 +19,57 @@ export type RephraseQuestionInput = {
 
 // tag::function[]
 export default function initRephraseChain(llm: BaseChatModel) {
-    // TODO: Create Prompt template
-    // const rephraseQuestionChainPrompt = PromptTemplate.fromTemplate<RephraseQuestionInput, string>(...)
-    // TODO: Create Runnable Sequence
-    // return RunnableSequence.from<RephraseQuestionInput, string>(
+    const rephraseQuestionChainPrompt = PromptTemplate.fromTemplate<RephraseQuestionInput, string>(`
+Given the following conversation and a question,
+rephrase the follow-up question to be a standalone question about the
+subject of the conversation history.
+
+If you do not have the required information required to construct
+a standalone question, ask for clarification.
+
+Always include the subject of the history in the question.
+
+History:
+{history}
+
+Question:
+{input}
+`)
+
+    return RunnableSequence.from<RephraseQuestionInput, string>([
+        RunnablePassthrough.assign({
+            history: ({ history }): string => {
+                if (history.length === 0) {
+                    return 'No history'
+                }
+                return history
+                    .map(
+                        (response: ChatbotResponse) =>
+                            `Human: ${response.input}\nAI: ${response.output}`,
+                    )
+                    .join('\n')
+            },
+        }),
+        rephraseQuestionChainPrompt,
+        llm,
+        new StringOutputParser(),
+    ])
 }
 // end::function[]
 
 /**
  * How to use this chain in your application:
 
-// tag::usage[]
-const llm = new OpenAI() // Or the LLM of your choice
-const rephraseAnswerChain = initRephraseChain(llm)
+ // tag::usage[]
+ const llm = new OpenAI() // Or the LLM of your choice
+ const rephraseAnswerChain = initRephraseChain(llm)
 
-const output = await rephraseAnswerChain.invoke({
-  input: 'What else did they act in?',
-  history: [{
-    input: 'Who played Woody in Toy Story?',
-    output: 'Tom Hanks played Woody in Toy Story',
-  }]
-}) // Other than Toy Story, what movies has Tom Hanks acted in?
-// end::usage[]
+ const output = await rephraseAnswerChain.invoke({
+ input: 'What else did they act in?',
+ history: [{
+ input: 'Who played Woody in Toy Story?',
+ output: 'Tom Hanks played Woody in Toy Story',
+ }]
+ }) // Other than Toy Story, what movies has Tom Hanks acted in?
+ // end::usage[]
  */
