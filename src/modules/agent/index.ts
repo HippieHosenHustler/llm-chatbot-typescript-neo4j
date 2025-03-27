49d23dd783722ef57511c782a1411a10c7096033
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2025-03-27 10:02:01 +0100

    chat actually interacts with the agent

--- src/modules/agent/index.ts
+++ src/modules/agent/index.ts
@@ -1,13 +1,27 @@
-import { ChatOpenAI } from '@langchain/openai'
-import { OpenAIEmbeddings } from '@langchain/openai'
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import initAgent from './agent'
 import { initGraph } from '../graph'
-import { sleep } from '@/utils'
 
 // tag::call[]
 export async function call(input: string, sessionId: string): Promise<string> {
-    // TODO: Replace this code with an agent
-    await sleep(2000)
-    return input
+    const llm = new ChatOpenAI({
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        configuration: {
+            baseURL: process.env.OPENAI_API_BASE,
+        },
+    })
+    const embeddings = new OpenAIEmbeddings({
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        configuration: {
+            baseURL: process.env.OPENAI_API_BASE,
+        },
+    })
+
+    // Get Graph Singleton
+    const graph = await initGraph()
+
+    const agent = await initAgent(llm, embeddings, graph)
+    return await agent.invoke({ input }, { configurable: { sessionId } })
 }
+
 // end::call[]
